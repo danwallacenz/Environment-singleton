ced5b2362a393cc4dd19ec96f89255d9e69ecc12
Dan Wallace <daniel.wallace.nz@gmail.com>
2019-09-19 12:59:41 +1200

    init

--- Repos/SceneDelegate.swift
+++ Repos/SceneDelegate.swift
@@ -8,16 +8,22 @@
 
 import UIKit
 
-class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+@UIApplicationMain
+class SceneDelegate: UIResponder, UIWindowSceneDelegate, UIApplicationDelegate {
 
     var window: UIWindow?
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+ 
+        guard let winScene = (scene as? UIWindowScene) else { return }
+
+        let vc = ReposViewController()
+ 
+        let win = UIWindow(windowScene: winScene)
+        win.rootViewController = vc
+        win.makeKeyAndVisible()
+        window = win
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
